5257899afa0baebf49db08577622d5f49a335997
Salvatore Agri <salvatore.agri@swisstxt.ch>
2022-02-25 17:56:33 +0100

    next auth test added

--- process.d.ts
+++ process.d.ts
@@ -12,5 +12,8 @@ declare namespace NodeJS {
     GOOGLE_SECRET: string
     AUTH0_ID: string
     AUTH0_SECRET: string
+    AZURE_AD_CLIENT_ID: string
+    AZURE_AD_CLIENT_SECRET: string
+    AZURE_AD_TENANT_ID: string
   }
 }
